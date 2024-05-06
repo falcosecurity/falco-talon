b6df5c760e584e99dc7f767bc13b67b4bac5b56b
Igor Eulalio <igor.eulalio@sysdig.com>
2024-05-06 12:35:36 -0300

    feat: refactoring drain.go to use helper function, creating helper function, refactoring terminate to use same helper function for ignoring pods validation

--- actionners/kubernetes/drain/drain.go
+++ actionners/kubernetes/drain/drain.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"github.com/falco-talon/falco-talon/internal/events"
 	kubernetes "github.com/falco-talon/falco-talon/internal/kubernetes/client"
+	"github.com/falco-talon/falco-talon/internal/kubernetes/helpers"
 	"github.com/falco-talon/falco-talon/internal/rules"
 	"github.com/falco-talon/falco-talon/utils"
 	"github.com/go-playground/validator/v10"
+	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -35,39 +36,31 @@ type Config struct {
 func Action(action *rules.Action, event *events.Event) (utils.LogLine, error) {
 	podName := event.GetPodName()
 	namespace := event.GetNamespaceName()
-
-	objects := map[string]string{
-		"pod":       podName,
-		"namespace": namespace,
-	}
+	objects := map[string]string{}
 
 	parameters := action.GetParameters()
 	gracePeriodSeconds := new(int64)
-	if parameters["grace_period_seconds"] != nil {
-		*gracePeriodSeconds = int64(parameters["grace_period_seconds"].(int))
+	if val, ok := parameters["grace_period_seconds"].(int); ok {
+		*gracePeriodSeconds = int64(val)
 	}
 
 	client := kubernetes.GetClient()
 	pod, err := client.GetPod(podName, namespace)
 	if err != nil {
-		objects["pod"] = podName
-		objects["namespace"] = namespace
 		return utils.LogLine{
-				Objects: objects,
-				Error:   err.Error(),
-				Status:  "failure",
-			},
-			err
+			Objects: objects,
+			Error:   err.Error(),
+			Status:  "failure",
+		}, err
 	}
 
 	node, err := client.GetNodeFromPod(pod)
 	if err != nil {
 		return utils.LogLine{
-				Objects: objects,
-				Error:   err.Error(),
-				Status:  "failure",
-			},
-			err
+			Objects: objects,
+			Error:   err.Error(),
+			Status:  "failure",
+		}, err
 	}
 
 	objects["node"] = node.Name
@@ -75,139 +68,77 @@ func Action(action *rules.Action, event *events.Event) (utils.LogLine, error) {
 	pods, err := client.Clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
 		FieldSelector: fmt.Sprintf("spec.nodeName=%s", node.GetName()),
 	})
+	if err != nil {
+		return utils.LogLine{
+			Objects: objects,
+			Error:   err.Error(),
+			Status:  "failure",
+		}, err
+	}
+
+	var aggregatedLog utils.LogLine
+	aggregatedLog.Objects = make(map[string]string)
+	var allErrors []string
+	var ignoredPods []string
 
 	for _, p := range pods.Items {
-
-		if parameters["ignore_daemonsets"] != nil || parameters["ignore_statefulsets"] != nil || parameters["min_healthy_replicas"] != nil {
-			pod, err := client.GetPod(p.Name, p.Namespace)
-			if err != nil {
-				return utils.LogLine{
-						Objects: objects,
-						Error:   err.Error(),
-						Status:  "failure",
-					},
-					err
-			}
-
-			if len(pod.OwnerReferences) != 0 {
-				switch pod.OwnerReferences[0].Kind {
-				case "DaemonSet":
-					if parameters["ignore_daemonsets"].(bool) {
-						return utils.LogLine{
-								Objects: objects,
-								Result:  fmt.Sprintf("the pod %v in the namespace %v belongs to a Daemonset and ignore_daemonsets is true", p.Name, p.Namespace),
-								Status:  "ignored",
-							},
-							nil
-					}
-				case "StatefulSet":
-					if parameters["ignore_statefulsets"].(bool) {
-						return utils.LogLine{
-								Objects: objects,
-								Result:  fmt.Sprintf("the pod %v in the namespace %v belongs to a Statefulset and ignore_statefulsets is true", p.Name, p.Namespace),
-								Status:  "ignored",
-							},
-							nil
-					}
-				case "ReplicaSet":
-					if parameters["min_healthy_replicas"] != nil {
-						u, errG := client.GetReplicasetFromPod(pod)
-						if errG != nil {
-							return utils.LogLine{
-									Objects: objects,
-									Error:   errG.Error(),
-									Status:  "failure",
-								},
-								errG
-						}
-						if u == nil {
-							return utils.LogLine{
-									Objects: objects,
-									Error:   fmt.Sprintf("can't find the replicaset for the pod %v in namespace %v", p.Name, p.Namespace),
-									Status:  "failure",
-								},
-								fmt.Errorf("can't find the replicaset for the pod %v in namespace %v", p.Name, p.Namespace)
-						}
-						if strings.Contains(fmt.Sprintf("%v", parameters["min_healthy_replicas"]), "%") {
-							v, _ := strconv.ParseInt(strings.Split(parameters["min_healthy_replicas"].(string), "%")[0], 10, 64)
-							if v > int64(100*u.Status.ReadyReplicas/u.Status.Replicas) {
-								return utils.LogLine{
-										Objects: objects,
-										Result:  fmt.Sprintf("not enough healthy pods in the replicaset of the pod %v in namespace %v", p.Name, p.Namespace),
-										Status:  "ignored",
-									},
-									fmt.Errorf("not enough healthy pods in the replicaset of the pod %v in namespace %v", p.Name, p.Namespace)
-							}
-						} else {
-							v, _ := strconv.ParseInt(parameters["min_healthy_replicas"].(string), 10, 64)
-							if v > int64(u.Status.ReadyReplicas) {
-								return utils.LogLine{
-										Objects: objects,
-										Result:  fmt.Sprintf("not enough healthy pods in the replicaset of the pod %v in namespace %v", p.Name, p.Namespace),
-										Status:  "ignored",
-									},
-									fmt.Errorf("not enough healthy pods in the replicaset of the pod %v in namespace %v", p.Name, p.Namespace)
-							}
-						}
-					}
-				}
-			}
+		line, err, ignored := helpers.LogIgnoredPods(parameters, client, p, objects)
+		if ignored {
+			// Append ignored pods info
+			aggregatedLog.Output += line.Output + "; "
+			ignoredPods = append(ignoredPods, p.Name)
+			continue
 		}
-
-		delOpts := metav1.DeleteOptions{GracePeriodSeconds: gracePeriodSeconds}
-		evictionGroupVersion, err := CheckEvictionSupport(client)
 		if err != nil {
-			return utils.LogLine{
-				Objects: objects,
-				Status:  "failure",
-				Error:   err.Error(),
-			}, err
+			allErrors = append(allErrors, err.Error())
+			continue
 		}
 
-		switch evictionGroupVersion {
-		case policyv1.SchemeGroupVersion:
-			// send policy/v1 if the server supports it
-			eviction := &policyv1.Eviction{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      pod.Name,
-					Namespace: pod.Namespace,
-				},
-				DeleteOptions: &delOpts,
-			}
-			err := client.PolicyV1().Evictions(eviction.Namespace).Evict(context.TODO(), eviction)
-			if err != nil {
-				return utils.LogLine{
-					Objects: objects,
-					Status:  "failure",
-					Error:   err.Error(),
-				}, err
-			}
-
-		default:
-			// otherwise, fall back to policy/v1beta1, supported by all servers that support the eviction subresource
-			eviction := &policyv1beta1.Eviction{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      pod.Name,
-					Namespace: pod.Namespace,
-				},
-				DeleteOptions: &delOpts,
-			}
-			err := client.Clientset.PolicyV1beta1().Evictions(p.Namespace).Evict(context.Background(), eviction)
-			if err != nil {
-				return utils.LogLine{
-					Objects: objects,
-					Status:  "failure",
-					Error:   err.Error(),
-				}, err
-			}
+		// Eviction logic if the pod is not ignored
+		err = performEviction(client, p, gracePeriodSeconds)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
 		}
 	}
-	return utils.LogLine{
-			Objects: objects,
-			Output:  fmt.Sprintf("the node '%v' has been drained", podName),
-			Status:  "success",
-		},
-		nil
+
+	if len(allErrors) > 0 {
+		aggregatedLog.Error = strings.Join(allErrors, "; ")
+		aggregatedLog.Status = "failure"
+	} else {
+		aggregatedLog.Status = "success"
+		aggregatedLog.Output = fmt.Sprintf("Node '%v' drained, ignored pods: %v", node.Name, strings.Join(ignoredPods, ", "))
+	}
+
+	return aggregatedLog, nil
+}
+
+func performEviction(client *kubernetes.Client, pod corev1.Pod, gracePeriodSeconds *int64) error {
+	delOpts := metav1.DeleteOptions{GracePeriodSeconds: gracePeriodSeconds}
+	evictionGroupVersion, err := CheckEvictionSupport(client)
+	if err != nil {
+		return err
+	}
+
+	switch evictionGroupVersion {
+	case policyv1.SchemeGroupVersion:
+		eviction := &policyv1.Eviction{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      pod.Name,
+				Namespace: pod.Namespace,
+			},
+			DeleteOptions: &delOpts,
+		}
+		return client.PolicyV1().Evictions(eviction.Namespace).Evict(context.TODO(), eviction)
+	default:
+		eviction := &policyv1beta1.Eviction{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      pod.Name,
+				Namespace: pod.Namespace,
+			},
+			DeleteOptions: &delOpts,
+		}
+		return client.Clientset.PolicyV1beta1().Evictions(pod.Namespace).Evict(context.Background(), eviction)
+	}
 }
 
 func CheckParameters(action *rules.Action) error {
