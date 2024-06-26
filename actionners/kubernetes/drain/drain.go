f2811be288bd072bd395020f8ab30dfdd9102fa2
Igor Eulalio <igor.eulalio@sysdig.com>
2024-06-25 21:12:48 -0300

    create watcher for eviction worker with timeout option

--- actionners/kubernetes/drain/drain.go
+++ actionners/kubernetes/drain/drain.go
@@ -2,8 +2,10 @@ package drain
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sync"
+	"time"
 
 	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -21,14 +23,18 @@ const (
 	EvictionKind = "Eviction"
 	// EvictionSubresource represents the kind of evictions object as pod's subresource
 	EvictionSubresource = "pods/eviction"
+	// HoldEvictionDefaultTickerSeconds represents how much time does the ticker takes to create another request to check if pods were evicted
+	HoldEvictionDefaultTickerSeconds = 5
 )
 
 type Config struct {
-	MinHealthyReplicas string `mapstructure:"min_healthy_replicas" validate:"omitempty,is_absolut_or_percent"`
-	IgnoreErrors       bool   `mapstructure:"ignore_errors" validate:"omitempty"`
-	IgnoreDaemonsets   bool   `mapstructure:"ignore_daemonsets" validate:"omitempty"`
-	IgnoreStatefulSets bool   `mapstructure:"ignore_statefulsets" validate:"omitempty"`
-	GracePeriodSeconds int    `mapstructure:"grace_period_seconds" validate:"omitempty"`
+	MinHealthyReplicas         string   `mapstructure:"min_healthy_replicas" validate:"omitempty,is_absolut_or_percent"`
+	IgnoreErrors               bool     `mapstructure:"ignore_errors" validate:"omitempty"`
+	IgnoreDaemonsets           bool     `mapstructure:"ignore_daemonsets" validate:"omitempty"`
+	IgnoreStatefulSets         bool     `mapstructure:"ignore_statefulsets" validate:"omitempty"`
+	GracePeriodSeconds         int      `mapstructure:"grace_period_seconds" validate:"omitempty"`
+	WaitPeriod                 int      `mapstructure:"wait_period" validate:"omitempty"`
+	WaitTimeExcludedNamespaces []string `mapstructure:"wait_time_excluded_namespaces" validate:"omitempty"`
 }
 
 func Action(action *rules.Action, event *events.Event) (utils.LogLine, *model.Data, error) {
@@ -174,6 +180,16 @@ func Action(action *rules.Action, event *events.Event) (utils.LogLine, *model.Da
 	}
 
 	wg.Wait()
+	if config.WaitPeriod != 0 {
+		err = verifyEvictionHasFinished(client, config.WaitPeriod, nodeName, config)
+		if err != nil {
+			return utils.LogLine{
+				Objects: objects,
+				Error:   fmt.Sprintf("pods were not evited during the wait period of %v seconds for node %s.", config.WaitPeriod, nodeName),
+				Status:  "failure",
+			}, nil, fmt.Errorf("pods were not evited during the wait period of %v seconds for node %s", config.WaitPeriod, nodeName)
+		}
+	}
 
 	if config.IgnoreErrors || (evictionErrorsCount == 0 && otherErrorsCount == 0) {
 		return utils.LogLine{
@@ -187,6 +203,52 @@ func Action(action *rules.Action, event *events.Event) (utils.LogLine, *model.Da
 		Error:   fmt.Sprintf("the node '%v' has not been fully drained: %v pods ignored, %v eviction errors, %v other errors", nodeName, ignoredPodsCount, evictionErrorsCount, otherErrorsCount),
 		Status:  "failure",
 	}, nil, fmt.Errorf("the node '%v' has not been fully drained: %v eviction errors, %v other errors", nodeName, evictionErrorsCount, otherErrorsCount)
+}
+
+func verifyEvictionHasFinished(c *kubernetes.Client, period int, nodeName string, config Config) error {
+	timeout := time.After(time.Duration(period) * time.Second)
+	ticker := time.NewTicker(HoldEvictionDefaultTickerSeconds * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			return errors.New("timeout reached before eviction finished")
+		case <-ticker.C:
+
+			var nonDaemonSetPods []string
+			excludedNamespaces := make(map[string]bool)
+			for _, namespace := range config.WaitTimeExcludedNamespaces {
+				excludedNamespaces[namespace] = true
+			}
+			pods, err := c.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
+				FieldSelector: fmt.Sprintf("spec.nodeName=%s", nodeName),
+			})
+			if err != nil {
+				return err
+			}
+
+			for _, pod := range pods.Items {
+				isDaemonSet := false
+				if pod.OwnerReferences != nil {
+					for _, ownerRef := range pod.OwnerReferences {
+						if ownerRef.Kind == "DaemonSet" {
+							isDaemonSet = true
+							break
+						}
+					}
+				}
+
+				if !isDaemonSet && !excludedNamespaces[pod.Namespace] {
+					nonDaemonSetPods = append(nonDaemonSetPods, pod.Name)
+				}
+
+				if len(nonDaemonSetPods) == 0 || nonDaemonSetPods == nil {
+					return nil
+				}
+			}
+		}
+	}
 }
 
 func CheckParameters(action *rules.Action) error {
